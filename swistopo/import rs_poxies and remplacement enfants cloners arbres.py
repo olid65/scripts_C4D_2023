db84205b6805df839df9f6c9613cea579fb1d709
Olivier Donzé <olivier.donze@hesge.ch>
2023-02-09 11:14:49 +0100

    convert phgysical sky to rs sky and sun + modif import rs proxies

--- swistopo/import rs_poxies and remplacement enfants cloners arbres.py
+++ swistopo/import rs_poxies and remplacement enfants cloners arbres.py
@@ -18,7 +18,7 @@ def getAllcloners(obj,start,lst = []):
     return lst
 
 def main() -> None:
-    
+
     lst_cloners =[]
     for o in doc.GetActiveObjects(0):
         getAllcloners(o,start = o,lst = lst_cloners)
@@ -27,12 +27,12 @@ def main() -> None:
         return
 
     first_object = doc.GetFirstObject()
-    
+
     path = '/Users/olivierdonze/switchdrive/C4D/BIBLIOTHEQUE/Redshift_proxies'
-    
+
     for fn in sorted(glob(os.path.join(path,'*.rs'))):
         c4d.documents.MergeDocument(doc, fn, c4d.SCENEFILTER_OBJECTS, None)
-    
+
     #liste des proxies
     lst_proxies = []
     obj = doc.GetFirstObject()
@@ -40,30 +40,45 @@ def main() -> None:
         if obj == first_object : break
         obj.SetBit(c4d.BIT_ACTIVE)
         lst_proxies.append(obj)
-        obj = obj.GetNext()  
-    
+        obj = obj.GetNext()
+
     if not lst_proxies :
         print('pas de proxies')
         return
-    
+
     doc.StartUndo()
+    dic_old_trees = {}
+    archives_trees = c4d.BaseObject(c4d.Onull)
+    archives_trees.SetName('old_trees')
+    archives_trees[c4d.ID_BASEOBJECT_VISIBILITY_EDITOR] = c4d.OBJECT_OFF
+    archives_trees[c4d.ID_BASEOBJECT_VISIBILITY_RENDER] = c4d.OBJECT_OFF
+    
     for cloner in lst_cloners:
-        #on efface les enfants
+        #on efface les enfants ou on déplace dans archive si pas déjà fait
         for child in cloner.GetChildren():
-            doc.AddUndo(c4d.UNDOTYPE_DELETEOBJ,child)
-            child.Remove()
+            name = child.GetName()
+            if not dic_old_trees.get(name,None):
+                dic_old_trees[name] = child
+                doc.AddUndo(c4d.UNDOTYPE_CHANGE,child)
+                child.InsertUnderLast(archives_trees)
+            else:
+                doc.AddUndo(c4d.UNDOTYPE_DELETEOBJ,child)
+                child.Remove()
         #et on met un clone de cahque proxy
         for proxy in lst_proxies:
             clone = proxy.GetClone(c4d.COPYFLAGS_NONE)
             clone.InsertUnderLast(cloner)
             doc.AddUndo(c4d.UNDOTYPE_NEWOBJ,clone)
     
-    #on efface les poxies de base
+    doc.InsertObject(archives_trees)
+    doc.AddUndo(c4d.UNDOTYPE_NEWOBJ,archives_trees)
+
+    #on efface les proxies de base
     for proxy in lst_proxies:
         proxy.Remove()
-            
+
     doc.EndUndo()
-    
+
     c4d.EventAdd()
 
 """
